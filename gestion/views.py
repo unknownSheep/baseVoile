1ca2336c940e77721ab6ccd3688b2d6d484221f0
Silvia de Beer <silvia@sogilis.com>
2024-02-24 15:01:18 +0100

    add styling color of the menubar

--- gestion/views.py
+++ gestion/views.py
@@ -42,6 +42,7 @@ def emprunts(request): # TODO: retour + degats et mise en page du form de l'ajou
         retourForm = RetourForm()
 
     context = {
+        "pageName": "emprunts",
         "nouvelEmpruntForm": nouvelEmpruntForm,
         "retourForm": retourForm,
         "sortis":  Emprunt.objects.filter(returnTime__isnull=True).order_by('startTime'),
@@ -54,20 +55,28 @@ def emprunts(request): # TODO: retour + degats et mise en page du form de l'ajou
 
 
 def gear(request):  # TODO: sorting + add pictures + add new form
-    context = {"materiel": Gear.objects.all()}
+    context = {"materiel": Gear.objects.all(),
+               "pageName": "materiel",
+               }
     return render(request, "gestion/materiel.html", context)
 
 
 def adherents(request):  # TODO:  sorting
-    context = {"adherents": Adherent.objects.all()}
+    context = {"adherents": Adherent.objects.all(),
+               "pageName": "adherents",
+               }
     return render(request, "gestion/adherents.html", context)
 
 
 def repair(request): # TODO: sorting + Repaired
-    context = {"reparer": Gear.objects.filter(toRepair=True)}
+    context = {"reparer": Gear.objects.filter(toRepair=True),
+               "pageName": "repair",
+               }
     return render(request, "gestion/reparations.html", context)
 
 
 def history(request): # TODO: sorting and filtering
-    context = {"emprunts": Emprunt.objects.filter(returnTime__isnull=False).filter(startTime__year=timezone.now().year).order_by('returnTime')}
+    context = {"emprunts": Emprunt.objects.filter(returnTime__isnull=False).filter(startTime__year=timezone.now().year).order_by('returnTime'),
+               "pageName": "historique",
+               }
     return render(request, "gestion/historique.html", context)
